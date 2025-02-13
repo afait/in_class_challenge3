f8984a9c5f987f43f8cddd6369b6a96de32b9d70
Annabelle Fait <imannaf8@gmail.com>
2025-02-13 16:25:07 -0500

    log final code

--- cc_example.js
+++ cc_example.js
@@ -11,3 +11,58 @@
 // }
 
 // console.log(findOrder(orders, 102));
+
+// Task 6
+
+// let inventory = {
+//     items: [],
+//     addItem(name, quantity) {
+//         this.items.push({name, quantity});
+//     },
+//     removeLatestItem(){
+//         this.items.pop();
+//     },
+//     removeFirstItem(){
+//         this.items.shift()
+//     }
+// };
+
+// inventory.addItem("Monitor", 5);
+// inventory.addItem("Keyboard", 10);
+// inventory.addItem("Mouse", 3);
+// console.log(JSON.stringify(inventory.items));
+// inventory.removeLatestItem();
+// console.log(JSON.stringify(inventory.items));
+// inventory.removeFirstItem();
+// console.log(JSON.stringify(inventory.items));
+
+// Task 7
+
+// let employees = [
+//         {name: "Alice", position: "Developer", salary: 70000},
+//         {name: "Bob", position: "Designer", salary: 60000},
+//         {name: "Charlie", position: "Manager", salary: 90000}
+//     ];
+
+
+// function findEmployee(employees, name) {
+//     return employees.find(employee => employee.name === name);
+// }
+
+// console.log(findEmployee(employees, 'Bob'));
+
+// Task 8
+
+// let orders1 = [{ id: 1, customer: "Alice"}, { id: 2, customer: "Bob"}];
+// let orders2 = [{ id: 3, customer: "Charlie"}, { id: 4, customer: "David"}];
+
+// function mergeOrders(orderA, orderB) {
+//     return [...orderA, ...orderB];
+// }
+// console.log(mergeOrders(orders1, orders2));
+
+// EXTRA:
+
+let employee = { name: "Alice", position: "Developer", salary: 70000};
+
+console.log(`${employee.name} is a ${employee.position} and makes $${employee.salary}`);
