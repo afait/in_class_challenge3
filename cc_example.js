d9c878c3849fa7ac94699eee968324b4e5c64041
Annabelle Fait <imannaf8@gmail.com>
2025-02-13 15:47:00 -0500

    Task 5

--- cc_example.js
+++ cc_example.js
@@ -1,1 +1,13 @@
 // Task 5
+
+// let orders = [
+//     {id: 101, customer: "Alice", total: 300},
+//     {id: 102, customer: "Bob", total: 450},
+//     {id: 103, customer: "Charlie", total: 200}
+// ];
+
+// function findOrder(orders, orderID) {
+//     return orders.find(order => order.id === orderID);
+// }
+
+// console.log(findOrder(orders, 102));
